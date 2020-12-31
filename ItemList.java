c7fa605c5b24b81b76541adb4059800fb64e44de
Kyle Sanquist <35445384+kSanquist@users.noreply.github.com>
2020-12-31 02:33:55 -0800

    Add files via upload

---
+++ ItemList.java
@@ -0,0 +1,8 @@
+public class ItemList
+{
+    // Add items
+    // Remove items
+    // Save shopping list (FileWriter)
+    // Display shopping list (toString)
+    // No duplicate items (HashSet)
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) {
+        // Allow for multiple shopping lists
+        // Allow for users to input commands
+        // Allow users to input shopping list item they want to add/remove
+        // Identified by different names
+
+
+    }
+}
